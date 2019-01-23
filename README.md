de21762af367f9eb2cc250c3d8f9fa95da3fe634
Mustafa  Tufan <mail@mustafatufan.com.tr>
2019-01-23 17:40:27 +0300

    re-initial

--- README.md
+++
@@ -1,7 +0,0 @@
-# btckit
-
-## Create Pair
-localhost:8090/api/createPair
-
-## Check Balance
-localhost:8090/api/getBalance?publicAddress=12UueGZMwLrEvYd1NgNTmkVUfy3svDHXt7
--- src/main/java/com/ionia/btckit/scheduler/service/SchedulerService.java
+++
@@ -1,9 +0,0 @@
-package com.ionia.btckit.scheduler.service;
-
-import com.ionia.btckit.service.PairServiceException;
-
-public interface SchedulerService {
-
-	public void generatePair() throws PairServiceException;
-
-}
