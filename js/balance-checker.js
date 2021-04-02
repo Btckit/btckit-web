961cb0c7563bdf26442f060a344b4bcb6aaf9ced
Mustafa Tufan <mustafa@tufan.ee>
2021-04-02 18:34:33 +0300

    deleted cors

--- js/balance-checker.js
+++ js/balance-checker.js
@@ -6,7 +6,7 @@ function check() {
 	let settings = {
 		"async": true,
 		"crossDomain": true,
-		"url": "https://cors-anywhere.herokuapp.com/http://api.btckit.io/getBalance?publicAddress=" + publicAddress.value,
+		"url": "http://api.btckit.io/getBalance?publicAddress=" + publicAddress.value,
 		"method": "GET",
 		"headers": {
 			"X-Requested-With": "XMLHttpRequest",
